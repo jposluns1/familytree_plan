e9de991eff9157ee0a92ecc2c8b4a7255caefe31
Raghu Betina <raghu@firstdraft.com>
2019-06-04 08:10:07 +0000

    Display only signed in users notes on index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,7 +10,7 @@ class NotesController < ApplicationController
   end
 
   def index
-    @notes = Note.all
+    @notes = current_user.notes.page(params[:page]).per(10)
 
     render("note_templates/index.html.erb")
   end
