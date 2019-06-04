389e880bad646b816ca46ff10bdce95a0b00705d
Raghu Betina <raghu@firstdraft.com>
2019-06-04 08:09:39 +0000

    Whitelist columns in RelationshipList for ActiveAdmin

--- app/admin/relationship_lists.rb
+++ app/admin/relationship_lists.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register RelationshipList do
+
+ permit_params :connection_id, :mother_id, :father_id, :child1_id, :child2_id, :child3_id, :child4_id, :child5_id, :step_mother_id, :step_father_id, :sibling1_id, :sibling2_id, :sibling3_id, :sibling4_id, :partner_id, :husband_id, :wife_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
