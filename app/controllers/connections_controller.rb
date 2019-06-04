18af5bca0906c9241801c8989e4d481fb253d795
Raghu Betina <raghu@firstdraft.com>
2019-06-04 08:10:07 +0000

    Only connection user can modify connection

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,4 +1,14 @@
 class ConnectionsController < ApplicationController
+  before_action :current_user_must_be_connection_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_connection_user
+    connection = Connection.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == connection.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @connections = Connection.all
 
